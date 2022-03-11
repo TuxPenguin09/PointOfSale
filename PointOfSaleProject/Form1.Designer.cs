7bb6919d93211943c0e503158c467bb95cc60036
alia manuba <tux.penguin.tab@gmail.com>
2022-03-12 00:03:13 +0800

    Initial Commit

---
+++ PointOfSaleProject/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace PointOfSaleProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SaleTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cartList = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(235, 179);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Chicken - P80";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(253, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(235, 179);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2 Pieces Chicken - P150";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(12, 197);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(235, 179);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Burger - P40";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(253, 197);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(235, 179);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "French Fries - P20";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button5.ForeColor = System.Drawing.Color.Black;
+            this.button5.Location = new System.Drawing.Point(12, 382);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(235, 179);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Pizza - P70";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button6.BackgroundImage")));
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button6.ForeColor = System.Drawing.Color.Black;
+            this.button6.Location = new System.Drawing.Point(253, 382);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(235, 179);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Hotdog - P15";
+            this.button6.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button7.BackgroundImage")));
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button7.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button7.ForeColor = System.Drawing.Color.Black;
+            this.button7.Location = new System.Drawing.Point(12, 567);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(235, 179);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Baked Potato - P65";
+            this.button7.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button8.BackgroundImage")));
+            this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button8.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button8.ForeColor = System.Drawing.Color.Black;
+            this.button8.Location = new System.Drawing.Point(253, 567);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(235, 179);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Mom\'s Spaghetti- P55";
+            this.button8.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button9.BackgroundImage")));
+            this.button9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button9.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button9.ForeColor = System.Drawing.Color.Black;
+            this.button9.Location = new System.Drawing.Point(12, 752);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(235, 179);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "Ribs- P155";
+            this.button9.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button10.BackgroundImage")));
+            this.button10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button10.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button10.ForeColor = System.Drawing.Color.Black;
+            this.button10.Location = new System.Drawing.Point(253, 752);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(235, 179);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Coke - P20";
+            this.button10.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // SaleTextBox
+            // 
+            this.SaleTextBox.Font = new System.Drawing.Font("Segoe UI", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SaleTextBox.Location = new System.Drawing.Point(516, 53);
+            this.SaleTextBox.Name = "SaleTextBox";
+            this.SaleTextBox.ReadOnly = true;
+            this.SaleTextBox.Size = new System.Drawing.Size(614, 96);
+            this.SaleTextBox.TabIndex = 10;
+            this.SaleTextBox.Text = "0.00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(480, -253);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 32);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Sale:";
+            // 
+            // cartList
+            // 
+            this.cartList.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cartList.FormattingEnabled = true;
+            this.cartList.ItemHeight = 28;
+            this.cartList.Location = new System.Drawing.Point(516, 219);
+            this.cartList.Name = "cartList";
+            this.cartList.Size = new System.Drawing.Size(624, 508);
+            this.cartList.TabIndex = 12;
+            this.cartList.SelectedIndexChanged += new System.EventHandler(this.cartList_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(516, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 32);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Cart:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(516, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 32);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Sale:";
+            // 
+            // button11
+            // 
+            this.button11.Enabled = false;
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button11.Location = new System.Drawing.Point(516, 743);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(196, 188);
+            this.button11.TabIndex = 15;
+            this.button11.Text = "Next Customer";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Enabled = false;
+            this.button12.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button12.Location = new System.Drawing.Point(718, 742);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(196, 188);
+            this.button12.TabIndex = 16;
+            this.button12.Text = "Enter Payment";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Enabled = false;
+            this.button13.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button13.Location = new System.Drawing.Point(920, 742);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(220, 188);
+            this.button13.TabIndex = 17;
+            this.button13.Text = "Compute Change";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1166, 942);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cartList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SaleTextBox);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "i8 resturant";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.TextBox SaleTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox cartList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+    }
+}
---
+++ PointOfSaleProject/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PointOfSaleProject
+{
+    public partial class Form1 : Form
+    {
+        public double SavedPrice = 0.0;
+        public double SavedPay = 0.0;
+        public bool cartlocked = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void FoodSelected(String foodandprice, double price)
+        {
+            double salefin = price + Convert.ToDouble(SaleTextBox.Text); 
+
+            SaleTextBox.Text = salefin.ToString();
+            cartList.Items.Add(foodandprice);
+            button12.Enabled = true;
+        }
+
+        void AllFoodButtons(Boolean set)
+        {
+            button1.Enabled = set;
+            button2.Enabled = set;
+            button3.Enabled = set;
+            button4.Enabled = set;
+            button5.Enabled = set;
+            button6.Enabled = set;
+            button7.Enabled = set;
+            button8.Enabled = set;
+            button9.Enabled = set;
+            button10.Enabled = set;
+        }
+
+        private void cartList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!cartlocked)
+            {
+                if (cartList.SelectedItems.Count != 0)
+                {
+                    while (cartList.SelectedIndex != -1)
+                    {
+                        //Console.WriteLine(cartList.GetItemText(cartList.SelectedItem));
+                        switch (cartList.GetItemText(cartList.SelectedItem))
+                        {
+                            case "Chicken - P80":
+                                double salefin = Convert.ToDouble(SaleTextBox.Text) - 80.0;
+                                SaleTextBox.Text = salefin.ToString();
+                                break;
+                            case "2 Pieces Chicken - P150":
+                                salefin = Convert.ToDouble(SaleTextBox.Text) - 150.0;
+                                SaleTextBox.Text = salefin.ToString();
+                                break;
+                            case "Burger - P40":
+                                salefin = Convert.ToDouble(SaleTextBox.Text) - 40.0;
+                                SaleTextBox.Text = salefin.ToString();
+                                break;
+                            case "French Fries - P20":
+                                salefin = Convert.ToDouble(SaleTextBox.Text) - 20.0;
+                                SaleTextBox.Text = salefin.ToString();
+                                break;
+                            case "Pizza - P70":
+                                salefin = Convert.ToDouble(SaleTextBox.Text) - 70.0;
+                                SaleTextBox.Text = salefin.ToString();
+                                break;
+                            case "Hotdog - P15":
+                                salefin = Convert.ToDouble(SaleTextBox.Text) - 15.0;
+                                SaleTextBox.Text = salefin.ToString();
+                                break;
+                            case "Baked Potato - P65":
+                                salefin = Convert.ToDouble(SaleTextBox.Text) - 65.0;
+                                SaleTextBox.Text = salefin.ToString();
+                                break;
+                            case "Mom's Spaghetti - P55":
+                                salefin = Convert.ToDouble(SaleTextBox.Text) - 55.0;
+                                SaleTextBox.Text = salefin.ToString();
+                                break;
+                            case "Ribs - P155":
+                                salefin = Convert.ToDouble(SaleTextBox.Text) - 155.0;
+                                SaleTextBox.Text = salefin.ToString();
+                                break;
+                            case "Coke - P20":
+                                salefin = Convert.ToDouble(SaleTextBox.Text) - 20.0;
+                                SaleTextBox.Text = salefin.ToString();
+                                break;
+                        }
+                        cartList.Items.RemoveAt(cartList.SelectedIndex);
+                        if (cartList.Items.Count == 0)
+                        {
+                            button12.Enabled = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FoodSelected("Chicken - P80", 80.0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FoodSelected("2 Pieces Chicken - P150", 150.0);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FoodSelected("Burger - P40", 40.0);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FoodSelected("French Fries - P20", 20.0);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FoodSelected("Pizza - P70", 70.0);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            FoodSelected("Hotdog - P15", 15.0);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            FoodSelected("Baked Potato - P65", 65.0);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            FoodSelected("Mom's Spaghetti - P55", 55.0);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            FoodSelected("Ribs - P155", 155.0);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            FoodSelected("Coke - P20", 20.0);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            SavedPrice = Convert.ToDouble(SaleTextBox.Text);
+
+            SaleTextBox.Text = "0.00";
+            label3.Text = "Enter Payment: (P" + SavedPrice.ToString() + ")";
+            SaleTextBox.ReadOnly = false;
+            button12.Enabled = false;
+            button13.Enabled = true;
+            button11.Text = "Reset";
+            button11.Enabled = true;
+            AllFoodButtons(false);
+            cartlocked = true;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            SavedPay = Convert.ToDouble(SaleTextBox.Text);
+
+            if (SavedPay < SavedPrice)
+            {
+                MessageBox.Show("The Payment is lower than the price!", "Lower than Price!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            } else
+            {
+                button13.Enabled = false;
+                button11.Enabled = true;
+                label3.Text = "Change: (Total Price: P" + SavedPrice.ToString() + ", Paid: P" + SavedPay.ToString() + ")";
+                SaleTextBox.ReadOnly = true;
+                SaleTextBox.Text = (SavedPay - SavedPrice).ToString();
+                button11.Text = "Next Customer";
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            label3.Text = "Sale:";
+            SaleTextBox.ReadOnly = true;
+            SaleTextBox.Text = "0.00";
+            button11.Text = "Next Customer";
+            button11.Enabled = false;
+            button12.Enabled = true;
+            button13.Enabled = false;
+            AllFoodButtons(true);
+            cartlocked = false;
+            cartList.Items.Clear();
+        }
+    }
+}
